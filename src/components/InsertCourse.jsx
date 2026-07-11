971b71403f59df5998ea47b306409dc07d53f9e9
Jyothika45 <jyothikakt55@gmail.com>
2026-07-11 14:43:20 +0530

    Insert

---
+++ src/components/InsertCourse.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+const InsertCourse = () => {
+  return (
+    <div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+<h3 align="center">INSERT COURSE</h3>
+              <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+<label htmlFor="coursename" className="form-label">Course Name :</label>
+<input type="text" className="form-control" />
+
+
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+<label htmlFor="duration" className="form-label">Duration :</label>
+<input type="text" className="form-control" />
+
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+<label htmlFor="fee" className="form-label ">Fee :</label>
+<input type="number" className="form-control" />
+
+
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+<label htmlFor="mode" className="form-label">Mode :</label>
+<select className="form-select">
+    <option>Select Mode</option>
+    <option>Online</option>
+    <option>Offline</option>
+    <option>Hybrid</option>
+</select>
+
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+<label htmlFor="trainer" className="form-label">Trainer :</label>
+<input type="text" className="form-control" />
+
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+    <button className="btn btn-success">INSERT</button>
+
+                </div>
+              </div>
+
+
+
+            </div>
+          </div>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default InsertCourse
